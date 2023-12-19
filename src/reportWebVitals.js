a597c9c621abd7c4ea761e208e26dccd7cfd9a7a
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-12-20 00:02:09 +0530

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,16 @@
+
+
+const domPreParse = new JSDOM(incident);
+<div class="sco" data-type="middle">
+    <div class="clear">
+        <span class="inc" data-type="home-icon"></span>
+        <span class="score" data-type="score">&nbsp;</span>
+        <span class="inc" data-type="away-icon">
+            <svg class="inc yellowcard"><use xlink:href="#icon-yellowcard"></use></svg>
+        </span>
+    </div>
+</div>
+//the below is the report web vitals data
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
