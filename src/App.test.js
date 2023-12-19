1e9d6bed93ee35be029f0a98335e3c07eb9cf233
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-12-19 23:31:02 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import {render,screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument(); //The jest-dom utility library provides this function  whether the element is present in the documnet or not 
 });
